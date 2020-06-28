aa49b3bba1cfdee2c4a3b495f3a26e84ef2ca62e
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-06-28 20:59:07 +1200

    Add contact me form that is hook up to google form

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,10 +1,13 @@
 window.onload = () => {
   const progress = document.getElementById("progress");
   const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const form = document.getElementById("contactForm");
 
   window.onscroll = () => {
     let scrollPosition = document.documentElement.scrollTop;
     let scrollProgress = (scrollPosition / height) * 100;
     progress.style.height = scrollProgress + "%";
   };
+
+  form.reset();
 };
